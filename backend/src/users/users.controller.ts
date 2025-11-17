c900254b007113e938986df9d9677697572a66e0
Ramlec <marcel_tessier@hotmail.com>
2025-11-17 18:38:23 +0100

    feat(users): add membership endpoints and refactor controller
    Reorganize imports and expand UsersController with new endpoints for
    managing users and memberships. Add createUser and deleteUser methods,
    and include routes to list a user's memberships, fetch a specific
    membership and fetch the currently membership. Use
    plainToInstance for transformation and return properlyserialized response DTOs. Also import ordering and updateJSDoc for clarity.
    
    These changes implement missing REST actions for user memberships and
    improve controller structure for maintainability and explicit response
    serialization.

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,14 +1,26 @@
-import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Patch, Post, Put } from "@nestjs/common";
-import { CreateOrUpdateUserDto } from "./dto/create-update-user.dto";
-import { UsersService } from "./users.service";
-import { UserEntity } from "./entities/user.entity";
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpException,
+    HttpStatus,
+    Param,
+    Patch,
+    Post,
+    Put,
+} from "@nestjs/common";
 import { plainToInstance } from "class-transformer";
-import { UserResponseDto } from "./dto/user-response.dto";
-import { PatchUserDto } from "./dto/patch-user.dto";
-import { GetUserDto } from "./dto/get-user.dto";
-import { GetUserMembershipDto } from "./dto/get-user-membership.dto";
-import { UserMembershipsService } from "src/user-memberships/user-memberships.service";
 import { UserMembershipResponseDto } from "src/user-memberships/dto/user-membership-response.dto";
+import { UserMembershipsService } from "src/user-memberships/user-memberships.service";
+
+import { CreateOrUpdateUserDto } from "./dto/create-update-user.dto";
+import { GetUserMembershipDto } from "./dto/get-user-membership.dto";
+import { GetUserDto } from "./dto/get-user.dto";
+import { PatchUserDto } from "./dto/patch-user.dto";
+import { UserResponseDto } from "./dto/user-response.dto";
+import { UserEntity } from "./entities/user.entity";
+import { UsersService } from "./users.service";
 
 @Controller(`users`)
 export class UsersController {
@@ -17,15 +29,45 @@ export class UsersController {
         private readonly userMembershipsService: UserMembershipsService,
     ) { }
 
+    /**
+     * Create a new user.
+     * @param createUserDto - The user to create.
+     * @returns The created user.
+     */
+    @Post()
+    async createUser(@Body() createUserDto: CreateOrUpdateUserDto): Promise<UserResponseDto> {
+        const newUser = plainToInstance(UserEntity, createUserDto);
+
+        const savedUser = await this.usersService.createUser(newUser);
+        return plainToInstance(UserResponseDto, savedUser, { excludeExtraneousValues: true });
+    }
 
     /**
-     * List all users.
-     * @returns All users.
+     * Delete a user.
+     * @param id - The id of the user.
+     * @returns The delete result.
      */
-    @Get()
-    async listUsers(): Promise<UserResponseDto[]> {
-        const users = await this.usersService.listUsers();
-        return plainToInstance(UserResponseDto, users, { excludeExtraneousValues: true });
+    @Delete(`:id`)
+    async deleteUser(@Param() { id }: GetUserDto): Promise<void> {
+        await this.usersService.deleteUser(id);
+    }
+
+    /**
+     * Get the active membership for a specific user (based on current date).
+     * @param id - The id of the user.
+     * @returns The active membership for this user, or null if no active membership.
+     */
+    @Get(`:id/memberships/active`)
+    async getActiveMembership(
+        @Param() { id }: GetUserDto,
+    ): Promise<null | UserMembershipResponseDto> {
+        const activeMembership = await this.userMembershipsService.getActiveMembership(id);
+        if (!activeMembership) {
+            return null;
+        }
+        return plainToInstance(UserMembershipResponseDto, activeMembership, {
+            excludeExtraneousValues: true,
+        });
     }
 
     /**
@@ -40,18 +82,46 @@ export class UsersController {
     }
 
     /**
-     * Create a new user.
-     * @param createUserDto - The user to create.
-     * @returns The created user.
+     * Get a specific membership for a user by userId and membershipId.
+     * @param id - The user id.
+     * @param membershipId - The membership id.
+     * @returns The user membership.
      */
-    @Post()
-    async createUser(
-        @Body() createUserDto: CreateOrUpdateUserDto
-    ): Promise<UserResponseDto> {
-        const newUser = plainToInstance(UserEntity, createUserDto);
+    @Get(`:id/memberships/:membershipId`)
+    async getUserMembershipByUserAndMembership(
+        @Param() { id, membershipId }: GetUserMembershipDto,
+    ): Promise<UserMembershipResponseDto> {
+        const userMembership =
+            await this.userMembershipsService.getUserMembershipByUserAndMembership(
+                id,
+                membershipId,
+            );
+        return plainToInstance(UserMembershipResponseDto, userMembership, {
+            excludeExtraneousValues: true,
+        });
+    }
 
-        const savedUser = await this.usersService.createUser(newUser);
-        return plainToInstance(UserResponseDto, savedUser, { excludeExtraneousValues: true });
+    /**
+     * Get all memberships for a specific user.
+     * @param id - The id of the user.
+     * @returns All memberships for this user.
+     */
+    @Get(`:id/memberships`)
+    async getUserMemberships(@Param() { id }: GetUserDto): Promise<UserMembershipResponseDto[]> {
+        const userMemberships = await this.userMembershipsService.getUserMemberships(id);
+        return plainToInstance(UserMembershipResponseDto, userMemberships, {
+            excludeExtraneousValues: true,
+        });
+    }
+
+    /**
+     * List all users.
+     * @returns All users.
+     */
+    @Get()
+    async listUsers(): Promise<UserResponseDto[]> {
+        const users = await this.usersService.listUsers();
+        return plainToInstance(UserResponseDto, users, { excludeExtraneousValues: true });
     }
 
     /**
@@ -63,7 +133,7 @@ export class UsersController {
     @Patch(`:id`)
     async patchUser(
         @Param() { id }: GetUserDto,
-        @Body() patchUserDto: PatchUserDto
+        @Body() patchUserDto: PatchUserDto,
     ): Promise<UserResponseDto> {
         const user: Partial<UserEntity> = plainToInstance(UserEntity, patchUserDto);
         const updatedUser = await this.usersService.patchUser(id, user);
@@ -82,11 +152,13 @@ export class UsersController {
     @Put(`:id`)
     async updateUser(
         @Param() { id }: GetUserDto,
-        @Body() updateUserDto: CreateOrUpdateUserDto
+        @Body() updateUserDto: CreateOrUpdateUserDto,
     ): Promise<UserResponseDto> {
         const user = plainToInstance(UserEntity, updateUserDto);
         const updatedUser = await this.usersService.updateOrCreateUser(id, user);
-        const response = plainToInstance(UserResponseDto, updatedUser, { excludeExtraneousValues: true });
+        const response = plainToInstance(UserResponseDto, updatedUser, {
+            excludeExtraneousValues: true,
+        });
 
         const isNew = updatedUser.createdAt.getTime() === updatedUser.updatedAt.getTime();
         if (isNew) {
@@ -94,54 +166,5 @@ export class UsersController {
         }
 
         return response;
-    }
-
-    /**
-     * Delete a user.
-     * @param id - The id of the user.
-     * @returns The delete result.
-     */
-    @Delete(`:id`)
-    async deleteUser(@Param() { id }: GetUserDto): Promise<void> {
-        await this.usersService.deleteUser(id);
-    }
-
-    /**
-     * Get all memberships for a specific user.
-     * @param id - The id of the user.
-     * @returns All memberships for this user.
-     */
-    @Get(`:id/memberships`)
-    async getUserMemberships(@Param() { id }: GetUserDto): Promise<UserMembershipResponseDto[]> {
-        const userMemberships = await this.userMembershipsService.getUserMemberships(id);
-        return plainToInstance(UserMembershipResponseDto, userMemberships, { excludeExtraneousValues: true });
-    }
-
-    /**
-     * Get the active membership for a specific user (based on current date).
-     * @param id - The id of the user.
-     * @returns The active membership for this user, or null if no active membership.
-     */
-    @Get(`:id/memberships/active`)
-    async getActiveMembership(@Param() { id }: GetUserDto): Promise<UserMembershipResponseDto | null> {
-        const activeMembership = await this.userMembershipsService.getActiveMembership(id);
-        if (!activeMembership) {
-            return null;
-        }
-        return plainToInstance(UserMembershipResponseDto, activeMembership, { excludeExtraneousValues: true });
-    }
-
-    /**
-     * Get a specific membership for a user by userId and membershipId.
-     * @param id - The user id.
-     * @param membershipId - The membership id.
-     * @returns The user membership.
-     */
-    @Get(`:id/memberships/:membershipId`)
-    async getUserMembershipByUserAndMembership(
-        @Param() { id, membershipId }: GetUserMembershipDto
-    ): Promise<UserMembershipResponseDto> {
-        const userMembership = await this.userMembershipsService.getUserMembershipByUserAndMembership(id, membershipId);
-        return plainToInstance(UserMembershipResponseDto, userMembership, { excludeExtraneousValues: true });
     }
 }
